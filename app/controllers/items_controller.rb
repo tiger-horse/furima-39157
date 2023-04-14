454d39d568fc5d59a7cceafc98513aad52bdf5fa
横尾和馬 <yokookazuma@yokookazumanoMacBook-Air.local>
2023-04-14 12:25:08 +0900

    商品出品機能(Rubocop)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,26 +4,22 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new()
+    @item = Item.new
   end
 
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path   
+      redirect_to root_path
     else
       render :new
-    end 
+    end
   end
 
   private
-    def item_params
-      params.require(:item).permit(:item_name, :content, :category_id, :condition_id, :postage_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id) 
-    end
 
-  
+  def item_params
+    params.require(:item).permit(:item_name, :content, :category_id, :condition_id, :postage_id, :prefecture_id,
+                                 :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
-
-
-      
-  
